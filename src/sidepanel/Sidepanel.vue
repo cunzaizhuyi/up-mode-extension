af30e22aca39cb309235a7a1d77afd8f80fe3502
cunzaizhuyi <877824709@qq.com>
2024-09-07 15:16:02 +0800

    fix: bug

--- src/sidepanel/Sidepanel.vue
+++ src/sidepanel/Sidepanel.vue
@@ -1,61 +1,145 @@
 <script setup lang="ts">
-import { extensionEnabled, noModifyCurrentSite } from '~/logic/storage'
-// import { sendMessage } from 'webext-bridge/content-script'
-// import browser from 'webextension-polyfill'
-import { addToWhitelist, removeFromWhitelist, isWhitelisted } from '~/contentScripts/whitelist-manager'
+import { setting } from '~/logic/storage'
+import { useWebExtensionStorage } from '~/composables/useWebExtensionStorage'
+import { sendMessage } from 'webext-bridge/popup'
+import browser from 'webextension-polyfill'
+import SwitchComponent from '~/components/Switch.vue';
+import ButtonComponent from '~/components/Button.vue';
 
-async function toggleExtension() {
-  console.log('isEnabled', extensionEnabled.value);
-  // 这句其实没用
-  // await sendMessage('toggle-extension', { extensionEnabled: extensionEnabled.value });
-}
+const extensions: any = useWebExtensionStorage('extensions', []);
 
-const currentHostname = ref('')
 onMounted(async () => {
   const [tab] = await browser.tabs.query({ active: true, currentWindow: true })
   if (tab.url) {
-    currentHostname.value = new URL(tab.url).hostname
-    noModifyCurrentSite.value = (await isWhitelisted(currentHostname.value))
   }
 })
 
-async function toggleCurrentSite() {
-  if (noModifyCurrentSite.value) {
-    await addToWhitelist(currentHostname.value)
+async function handleUpMode() {
+  hiddenExtensions();
+  await toggleBookmark()
+  window.close();
+  // setting.value.isFullscreen && toggleFullscreen(true)
+  // setting.value.isBookmark && toggleBookmark();
+}
+function handleReset() {
+  showExtensions();
+  window.close();
+  // setting.value.isFullscreen && toggleFullscreen(false)
+  // setting.value.isBookmark && toggleBookmark();
+}
+
+async function hiddenExtensions(){
+  extensions.value = (await browser.management.getAll())
+  .filter((extension)=> extension.enabled);
+  const currentExtensionId = browser.runtime.id;
+
+  // 禁用除当前扩展之外的所有扩展
+  extensions.value.forEach((extension: any) => {
+      if (extension.id !== currentExtensionId && extension.enabled) {
+          browser.management.setEnabled(extension.id, false);
+          // console.log(`禁用扩展: ${extension.name}`);
+      }
+  });
+  
+}
+
+async function showExtensions() {
+  const currentExtensionId = browser.runtime.id;
+  extensions.value.forEach((extension: any) => {
+    if (extension.id !== currentExtensionId) {
+      browser.management.setEnabled(extension.id, true);
+      // console.log(`启用扩展: ${extension.name}`);
+    }
+  });
+}
+
+async function toggleFullscreen(isFull: boolean) {
+  if (isFull) {
+    document.documentElement.requestFullscreen();
+    // const w = await browser.windows.getCurrent();
+    // browser.windows.update((w as any).id, { state: "fullscreen" });
   } else {
-    await removeFromWhitelist(currentHostname.value)
+    document?.exitFullscreen();
   }
+}
+
+async function toggleBookmark () {
+  const platformInfo = await browser.runtime.getPlatformInfo()
+  let keyEventOptions;
+  console.log('info', platformInfo);
+  if (platformInfo.os === "mac") {
+    keyEventOptions = {
+      key: 'b',
+      metaKey: true,  // Command key on Mac
+      shiftKey: true
+    };
+  } else {
+    keyEventOptions = {
+      key: 'b',
+      ctrlKey: true,
+      shiftKey: true
+    };
+  }
+  const [tab] = await browser.tabs.query({ active: true, currentWindow: true })
+  console.log('tabid', tab, tab?.id)
+  try {
+    browser.scripting.executeScript({
+    target: { tabId: tab?.id as number },
+    func: (options) => {
+      document.dispatchEvent(new KeyboardEvent('keydown', {}));
+    },
+    args: [keyEventOptions]
+  });
+  } catch (error) {
+    console.log('ee', error)
+  }
+  
 }
 </script>
 
 <template>
-  <main class="w-full px-4 py-5 text-gray-700">
-    <h1 class="text-lg font-bold mb-4">插件设置</h1>
+  <main class="w-[300px] px-4 py-5 text-gray-700">
+    <!-- 添加按钮 -->
+    <div class="flex justify-around mb-4">
+      <ButtonComponent bgColor="blue"
+       txtColor="white"
+       class="w-160px"
+        @click="handleUpMode">Hide All Extensions</ButtonComponent>
+      <ButtonComponent bgColor="gray"
+       txtColor="white"
+       class="w-80px"
+        @click="handleReset">Reset</ButtonComponent>
+    </div>
+
+    <!-- <h1 class="text-lg font-bold mb-4">Setting</h1>
     <form @submit.prevent>
-      <div class="flex items-center mb-4">
-        <input 
-          id="enable-extension" 
-          type="checkbox" 
-          v-model="extensionEnabled" 
-          @change="toggleExtension"
-          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-        >
-        <label for="enable-extension" class="ml-2 text-sm font-medium text-gray-900">
-          用新标签页打开链接
+      <div class="flex justify-around items-center mb-4">
+        <label for="enable-extension" class="mr-2 text-sm font-medium text-gray-900">
+          Toggle Extensions
         </label>
+        <SwitchComponent v-model="setting.isExtention" class="ml-2" />
       </div>
-      <div v-if="extensionEnabled" class="flex items-center mb-4">
-        <input 
-          id="modify-current-site" 
-          type="checkbox" 
-          v-model="noModifyCurrentSite"
-          @change="toggleCurrentSite"
-          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-        >
-        <label for="modify-current-site" class="ml-2 text-sm font-medium text-gray-900">
-          不修改本网站
+      <div class="flex justify-around items-center mb-4">
+        <label for="enable-extension" class="mr-2 text-sm font-medium text-gray-900">
+          Toggle Fullscreen
         </label>
+        <SwitchComponent v-model="setting.isFullscreen" class="ml-2" />
       </div>
-    </form>
+      <div class="flex justify-around items-center mb-4">
+        <label for="enable-extension" class="mr-2 text-sm font-medium text-gray-900">
+          Toggle Bookmarks
+        </label>
+        <SwitchComponent v-model="setting.isBookmark" class="ml-2" />
+      </div>
+    </form> -->
+
+    <!-- 添加 GitHub 链接 -->
+    <div class="mt-4 flex justify-center">
+      <a href="https://github.com/cunzaizhuyi/open-new-tab-extension" target="_blank" class="flex items-center text-blue-600 hover:underline">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" class="w-5 h-5 mr-2">
+          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.54 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.83-.01-1.5-2.24.49-2.71-1.08-2.71-1.08-.36-.91-.88-1.15-.88-1.15-.72-.49.05-.48.05-.48.8.06 1.22.82 1.22.82.71 1.22 1.86.87 2.31.67.07-.51.28-.87.51-1.07-1.78-.2-3.65-.89-3.65-3.95 0-.87.31-1.58.82-2.14-.08-.2-.36-1.02.08-2.12 0 0 .67-.22 2.2.84A7.66 7.66 0 0 1 8 2.5c.68 0 1.36.09 2 .26 1.53-1.06 2.2-.84 2.2-.84.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.14 0 3.07-1.87 3.75-3.66 3.95.29.25.55.74.55 1.49 0 1.08-.01 1.95-.01 2.21 0 .21.15.46.55.38C13.71 14.54 16 11.54 16 8c0-4.42-3.58-8-8-8z"/>
+        </svg>
+      </a>
+    </div>
   </main>
 </template>
